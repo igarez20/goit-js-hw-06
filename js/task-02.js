d826811900c732eeb84dd4e79d6ad49478d7a374
Ihor Rusyn <igarez20@gmail.com>
2023-03-04 17:26:22 +0200

    task 2 complete

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const listItems = [];
+
+ingredients.forEach((ingredient) => {
+  const ingredientsItem = document.createElement("li");
+  ingredientsItem.textContent = ingredient;
+  ingredientsItem.classList.add("item");
+  listItems.push(ingredientsItem);
+});
+
+ingredientsRef.append(...listItems);
