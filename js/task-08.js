29ce1fc16460a7cc61836999adb62b60c2965c02
Ihor Rusyn <igarez20@gmail.com>
2023-03-15 17:56:17 +0200

    task 8 complete

--- js/task-08.js
+++ js/task-08.js
@@ -3,4 +3,15 @@ const loginFormRef = document.querySelector(".login-form");
 loginFormRef.addEventListener("submit", (event) => {
   event.preventDefault();
   const userData = {};
+  for (const element of loginFormRef.elements) {
+    if (element.name) {
+      userData[element.name] = element.value;
+    }
+  }
+  if (userData.email && userData.password) {
+    console.log(userData);
+    loginFormRef.reset();
+  } else {
+    alert("Всі поля повинні бути заповнені");
+  }
 });
